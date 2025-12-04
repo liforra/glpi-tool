99934deac30c06a8c657c14f36b4749c0742e3a8
Liforra <git@liforra.de>
2025-12-04 14:44:55 +0100

    refactor: update setup.py for standard python package and remove cython

--- setup.py
+++ setup.py
@@ -1,34 +1,17 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import sys
-
-# Wir definieren die Python-Dateien, die wir kompilieren wollen.
-extensions = [
-    Extension(
-        "gui",          # <--- HIER GEÄNDERT
-        ["gui.py"],     # <--- HIER GEÄNDERT
-    ),
-    Extension(
-        "glpi",
-        ["glpi.py"],
-    ),
-    Extension(
-        "system_info",
-        ["system_info.py"],
-    ),
-]
-
-# Zusätzliche Compiler-Argumente
-compiler_args = []
-if sys.platform == "win32":
-    compiler_args.append("/WX-")
+from setuptools import setup, find_packages
 
 setup(
     name="GLPI GUI Client",
-    ext_modules=cythonize(
-        extensions,
-        compiler_directives={"language_level": "3"},
-        annotate=True,
-    ),
-    options={'build_ext': {'compiler_args': compiler_args}},
+    version="0.0.1", # This will be replaced by the workflow
+    packages=find_packages(), # This will find gui, glpi, system_info if they are in a package structure
+    py_modules=["gui", "glpi", "system_info"], # Explicitly list individual .py files at the root
+    install_requires=[
+        "toml",
+        "requests",
+        "psutil",
+        "Pillow",
+        "wmi; sys_platform == 'win32'",
+        "pywin32; sys_platform == 'win32'",
+        "sv-ttk"
+    ],
 )
